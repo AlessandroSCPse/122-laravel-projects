171e286f656a037edc5fc298043e409a3ac2f916
AlessandroSCPse <scolozzi.alessandro@gmail.com>
2024-05-31 15:55:20 +0200

    start crud

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h2>Projects</h2>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Client name</th>
+                <th scope="col">Created at</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+            @foreach ($projects as $project)
+                <tr>
+                    <td>{{ $project->id }}</td>
+                    <td>{{ $project->name }}</td>
+                    <td>{{ $project->client_name }}</td>
+                    <td>{{ $project->created_at }}</td>
+                    <td>
+                        <div>
+                            <a href="{{ route('admin.projects.show', ['project' => $project->id]) }}">View</a>
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+      </table>
+
+@endsection
